6be329d294e8a1860d046433ee55fac73a1fcab0
Dunc1995 <duncanbailey1995@gmail.com>
2024-03-22 15:52:30 +1300

    Adds ability to plot crypto value data. Still not sure how to specify timeframe via query

--- stupidinvestorbot/app.py
+++ stupidinvestorbot/app.py
@@ -1,11 +1,30 @@
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import stupidinvestorbot.crypto as crypto
 
+
 def get_time_series_data():
-    coin_summaries = crypto.get_top_ten_gaining_coins()
+    coin_number = 10
+    i = 0
+    coin_summaries = crypto.get_highest_gain_coins(coin_number)
+
+    fig, axs = plt.subplots(coin_number)
 
     for coin in coin_summaries:
         time_series_data = crypto.get_valuation(coin.instrument_name, 'mark_price')
 
-        with open(f'''{coin.instrument_name.lower()}_time_series.txt''', 'w') as f:
-            f.write(time_series_data)
-            f.close()
+        df = pd.DataFrame.from_dict(time_series_data['result']['data'])
+        df['t'] = df['t'].apply(lambda x: dt.datetime.fromtimestamp(x / 1000))
+        df['v'] = df['v'].astype(float)
+
+        print(df)
+
+        axs[i].plot(df['t'], df['v'])
+        axs[i].set_title(coin.instrument_name)
+        axs[i].xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y'))
+
+        i += 1
+
+    plt.show()
--- stupidinvestorbot/crypto.py
+++ stupidinvestorbot/crypto.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import datetime as dt
+import time
 import stupidinvestorbot.auth as auth
 from stupidinvestorbot import CRYPTO_REST_API
 from stupidinvestorbot.models import Ticker
@@ -10,17 +12,27 @@ def user_balance() -> str:
     return auth.post_request(1, 'user-balance')
 
 
-def get_top_ten_gaining_coins() -> list[Ticker]:
+def get_highest_gain_coins(number_of_coins : int) -> list[Ticker]:
     response = requests.get(api('get-tickers'))
 
     data = [ Ticker(obj) for obj in json.loads(response.text)['result']['data'] ]
 
-    result = sorted(data, key=lambda x: tuple(x.percentage_change_24h))[-10:]
+    result = sorted(data, key=lambda x: tuple(x.percentage_change_24h))[-number_of_coins:]
 
     return result
 
 
-def get_valuation(instrument_name : str, valuation_type : str):
-    response = requests.get(api(f'''get-valuations?instrument_name={instrument_name}&valuation_type={valuation_type}'''))
+def get_valuation(instrument_name : str, valuation_type : str) -> dict:
+    to_unix = lambda x : int(time.mktime(x.timetuple()) * 1000)
 
-    return response.text
+    date_now = to_unix(dt.datetime.now())
+    date_past = to_unix(dt.datetime.now() - dt.timedelta(days=1))
+
+    response = requests.get(api(f'''get-valuations?instrument_name={instrument_name}&valuation_type={valuation_type}&count=10000'''))
+
+    if (response.status_code != 200):
+        print(response.text)
+
+        raise ValueError('Unable to process data')
+
+    return json.loads(response.text)
